64af758c3b7c30f826a35d0e3e2cfd6d0b6cf3f5
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-03-21 22:51:40 -0300

    feat: add sidebox style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './styles/reset.css';
-import './styles/style.css';
+import "./styles/reset.css";
+import "./styles/style.css";
 import Header from "./components/Header";
+import HashtagBox from "./components/HashtagBox";
 
 export default function App() {
     return (
-        <Header />
+        <>
+            <Header />
+            <HashtagBox />
+        </>
     );
 }
