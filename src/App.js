7df5de3872665618a013d3bc38587c11b1948e9c
Vinicius <vinicunha2008@gmail.com>
2022-03-23 13:55:27 -0300

    app organized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./styles/reset.css";
 import "./styles/style.css";
-import Header from "./components/Header";
 import Timeline from "./components/Timeline";
 import HashtagBox from "./components/HashtagBox";
 
 export default function App() {
     return (
         <>
-            <Header />
             <Timeline />
-            <HashtagBox />
         </>
     );
 }
