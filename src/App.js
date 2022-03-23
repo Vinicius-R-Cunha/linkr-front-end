4637700a842e5380d1c83581e5f23316aaa0a7b3
kethllen <kety_andrey@hotmail.com>
2022-03-23 20:53:04 -0300

    autenticacao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './styles/reset.css';
-import './styles/style.css';
+import "./styles/reset.css";
+import "./styles/style.css";
 import Timeline from "./components/Timeline";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
+import { useEffect, useState } from "react";
 
 export default function App() {
-    return (
-        <Timeline />
-    );
+  const tokenOnLocalStorage = localStorage.getItem("token");
+
+  const [token, setToken] = useState(tokenOnLocalStorage);
+
+  function setAndPersistToken(token) {
+    setToken(token);
+    localStorage.setItem("token", token);
+  }
+  return (
+    <UserContext.Provider value={{ token, setToken, setAndPersistToken }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/timeline" element={<Timeline />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
