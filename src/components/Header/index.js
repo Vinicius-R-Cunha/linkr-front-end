fe9d112f98a6dbd2d83eb5245603b89a189a7ccd
kethllen <kety_andrey@hotmail.com>
2022-03-25 10:10:52 -0300

    50% pesquisa usuarios

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,73 @@
-import { HeaderDiv } from "./style"
-import { useState } from "react";
-import temp from "../../assets/perfil-temp.png"
-import { BsChevronDown, BsChevronUp } from 'react-icons/bs'
-import { HiOutlineSearch } from 'react-icons/hi'
+import { HeaderDiv, Debounce } from "./style";
+import { useState, useEffect } from "react";
+import temp from "../../assets/perfil-temp.png";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { HiOutlineSearch } from "react-icons/hi";
+import axios from "axios";
 
 export default function Header() {
-    const [showLogout, setShowLogout] = useState(false);
+  const [showLogout, setShowLogout] = useState(false);
+  const [users, setUsers] = useState();
+  const [search, setSearch] = useState();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    const promise = axios.get(process.env.REACT_APP_BACK_URL + "users", {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    promise.then((response) => {
+      setUsers(response.data);
+    });
+    promise.catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
-    return (
-        <HeaderDiv>
-            <p className="logo-name">linkr</p>
-            <div className="input-icon">
-                <input type="text" placeholder="Search for people" />
-                <HiOutlineSearch className="search-icon" />
-            </div>
-            <div className="icon-image">
-                {showLogout &&
-                    <>
-                        <BsChevronUp className="chevron-icon" onClick={() => setShowLogout(false)} />
-                        <div className="logout-button">Logout</div>
-                    </>
-                }
-                {!showLogout && <BsChevronDown className="chevron-icon" onClick={() => setShowLogout(true)} />}
-                <img src={temp} alt="" />
-            </div>
-        </HeaderDiv>
-    );
+  const getFilteredItems = (query, users) => {
+    if (!query) {
+      return;
+    }
+    return users.filter((user) => user.name.includes(query));
+  };
+
+  const filteredItems = getFilteredItems(search, users);
+  console.log(filteredItems);
+  return (
+    <HeaderDiv>
+      <p className="logo-name">linkr</p>
+      <div className="search">
+        <div className="input-icon">
+          <Debounce
+            minLength={3}
+            debounceTimeout={300}
+            type="text"
+            onChange={(event) => setSearch(event.target.value)}
+            placeholder="Search for people"
+          />
+          <HiOutlineSearch className="search-icon" />
+        </div>
+
+        <div className="searchContainer"></div>
+      </div>
+      <div className="icon-image">
+        {showLogout && (
+          <>
+            <BsChevronUp
+              className="chevron-icon"
+              onClick={() => setShowLogout(false)}
+            />
+            <div className="logout-button">Logout</div>
+          </>
+        )}
+        {!showLogout && (
+          <BsChevronDown
+            className="chevron-icon"
+            onClick={() => setShowLogout(true)}
+          />
+        )}
+        <img src={temp} alt="" />
+      </div>
+    </HeaderDiv>
+  );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,130 +1,162 @@
 import styled from "styled-components";
+import { DebounceInput } from "react-debounce-input";
 
 const HeaderDiv = styled.div`
-    width: 100%;
-    height: 72px;
+  width: 100%;
+  height: 72px;
 
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    background-color: #151515;
+  background-color: #151515;
 
-    position: fixed;
-    top: 0;
+  position: fixed;
+  top: 0;
+  z-index: 10;
 
-    .logo-name {
-        font-family: 'Passion One', cursive;
-        font-weight: 700;
-        font-size: 49px;
-        letter-spacing: 0.05em;
-        color: #FFFFFF;
-        
-        margin-left: 28px;
+  .logo-name {
+    font-family: "Passion One", cursive;
+    font-weight: 700;
+    font-size: 49px;
+    letter-spacing: 0.05em;
+    color: #ffffff;
 
-        cursor: pointer;
-    }
+    margin-left: 28px;
+
+    cursor: pointer;
+  }
+  .search {
+    position: relative;
 
     .input-icon {
-        position: relative;
+      position: relative;
 
-        display: flex;
-        align-items: center;
+      display: flex;
+      align-items: center;
 
-        input {
-            all: unset;
+      z-index: 1;
 
-            width: 563px;
-            height: 45px;
+      input {
+        all: unset;
 
-            font-family: 'Lato';
-            font-style: normal;
-            font-weight: 400;
-            font-size: 19px;
-            text-indent: 14px;
+        width: 563px;
+        height: 45px;
 
-            background: #FFFFFF;
-            border-radius: 8px;
+        font-family: "Lato";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19px;
+        text-indent: 14px;
 
-            ::placeholder {
-                color: #C6C6C6;
-            }
+        background: #ffffff;
+        border-radius: 8px;
+
+        ::placeholder {
+          color: #c6c6c6;
         }
+      }
 
-        .search-icon {
-            font-size: 23px;
-            color: #C6C6C6;
+      .search-icon {
+        font-size: 23px;
+        color: #c6c6c6;
 
-            position: absolute;
-            right: 11px;
-        }
+        position: absolute;
+        right: 11px;
+      }
+    }
+    .searchContainer {
+      position: absolute;
+      top: 40px;
+      width: 563px;
+      height: 200px;
+      background-color: green;
+    }
+  }
+
+  .icon-image {
+    display: flex;
+    align-items: center;
+
+    margin-right: 17px;
+
+    .chevron-icon {
+      font-size: 27px;
+      color: #ffffff;
+
+      margin-right: 12px;
+
+      cursor: pointer;
+    }
+
+    img {
+      width: 53px;
+      height: 53px;
+
+      border-radius: 26.5px;
+
+      cursor: pointer;
+    }
+
+    .logout-button {
+      width: 150px;
+      height: 47px;
+
+      font-family: "Lato";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 17px;
+      letter-spacing: 0.05em;
+      color: #ffffff;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      position: absolute;
+      top: 72px;
+      right: -17px;
+
+      background: #171717;
+      border-radius: 0px 0px 20px 20px;
+
+      cursor: pointer;
+    }
+  }
+
+  @media (max-width: 1200px) {
+    .search {
+      display: none;
+    }
+
+    .logo-name {
+      font-size: 45px;
+
+      margin-left: 17px;
     }
 
     .icon-image {
-        display: flex;
-        align-items: center;
-
-        margin-right: 17px;
-
-        .chevron-icon {
-            font-size: 27px;
-            color: #FFFFFF;
-
-            margin-right: 12px;
-
-            cursor: pointer;
-        }
-
-        img {
-            width: 53px;
-            height: 53px;
-
-            border-radius: 26.5px;
-
-            cursor: pointer;
-        }
-
-        .logout-button {
-            width: 150px;
-            height: 47px;
-
-            font-family: 'Lato';
-            font-style: normal;
-            font-weight: 700;
-            font-size: 17px;
-            letter-spacing: 0.05em;
-            color: #FFFFFF;
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-
-            position: absolute;
-            top: 72px;
-            right: -17px;
-
-            background: #171717;
-            border-radius: 0px 0px 20px 20px;
-
-            cursor: pointer;
-        }
+      margin-right: 14px;
     }
+  }
+`;
+const Debounce = styled(DebounceInput)`
+  all: unset;
 
-    @media (max-width: 1200px) {
-        .input-icon {
-            display: none;
-        }
-        
-        .logo-name {
-            font-size: 45px;
+  width: 563px;
+  height: 45px;
 
-            margin-left: 17px;
-        }
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 19px;
+  text-indent: 14px;
 
-        .icon-image {
-            margin-right: 14px;
-        }
-    }
-`
+  background: #ffffff;
+  border-radius: 8px;
 
-export { HeaderDiv }
+  ::placeholder {
+    color: #c6c6c6;
+  }
+`;
+export { HeaderDiv, Debounce };
