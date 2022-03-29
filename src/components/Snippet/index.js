51b56bb939287530a99e2ee5ee3c5193eaac3b64
abellasjuan <juanvictoraa@gmail.com>
2022-03-29 12:38:48 -0300

    fix: snippet str length

--- src/components/Snippet/index.js
+++ src/components/Snippet/index.js
@@ -10,7 +10,7 @@ export default function Snippet({ post }) {
     }
 
     function truncateString(str, num) {
-        if (str.length > num) {
+        if (str?.length > num) {
             return str.slice(0, num) + "...";
         } else {
             return str;
