bf7f1c4c5dee78c06ac60d03e2b3284dacdcde3f
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-03-31 13:42:22 -0300

    update feature

--- src/components/ScrollContainer/index.js
+++ src/components/ScrollContainer/index.js
@@ -1,6 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import api from "../../services/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function ScrollContainer({
     postsArray,
@@ -10,23 +10,30 @@ export default function ScrollContainer({
     children,
 }) {
     const [offset, setOffset] = useState(5);
-    const [loadMoreBoo, setLoadMoreBoo] = useState(true);
-    function handleLoadMore() {
+    const [loadMoreBoo, setLoadMoreBoo] = useState(false);
+    const [newPosts, setNewPosts] = useState([]);
+    console.log(loadMoreBoo);
+
+    useEffect(() => {
         const promise = api.getPosts(route + `?offset=${offset}`, token);
         promise.then((res) => {
-            console.log("????");
-            if (!res.data?.length) {
-                setLoadMoreBoo(false);
-                return;
+            if (res.data?.length === 0) {
+                return setLoadMoreBoo(false);
             }
             setOffset(offset + 5);
-            setPostsArray(postsArray?.concat(res.data));
+            setNewPosts([...res.data]);
+            setLoadMoreBoo(true);
         });
-        promise.catch((error) => console.log(error.response));
+        promise.catch((error) => setLoadMoreBoo(false));
+    }, []);
+
+    function handleLoadMore() {
+        setPostsArray(postsArray?.concat(newPosts));
     }
 
     return (
         <InfiniteScroll
+            threshold={1}
             pageStart={0}
             loadMore={handleLoadMore}
             hasMore={loadMoreBoo}
