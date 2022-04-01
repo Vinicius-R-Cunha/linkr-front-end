d610b0292ce18fd16ec951f6543600fe5c8e91f6
kethllen <kety_andrey@hotmail.com>
2022-04-01 15:51:44 -0300

    retirando console log

--- src/components/NewPostsNotificationBar/index.js
+++ src/components/NewPostsNotificationBar/index.js
@@ -5,43 +5,38 @@ import { useState } from "react";
 import useInterval from "use-interval";
 
 export default function NewPostsNotificationBar({
-    route,
-    token,
-    postsArray,
-    setPostsArray,
-    totalPosts,
-    setTotalPosts,
+  route,
+  token,
+  postsArray,
+  setPostsArray,
+  totalPosts,
+  setTotalPosts,
 }) {
-    const [newPosts, setNewPosts] = useState([]);
-    const [diff, setDiff] = useState(0);
-    console.log(newPosts.length, totalPosts.length);
-    console.log("Diff ", diff);
+  const [newPosts, setNewPosts] = useState([]);
+  const [diff, setDiff] = useState(0);
+  useInterval(() => {
+    const promise = api.getPosts(route + "?noLimit=true", token);
+    promise.then((res) => {
+      setNewPosts(res.data);
+      setDiff(res.data?.length - totalPosts.length);
+    });
+    promise.catch(() => setDiff(0));
+  }, 15000);
 
-    useInterval(() => {
-        const promise = api.getPosts(route + "?noLimit=true", token);
-        promise.then((res) => {
-            console.log(res.data);
-            setNewPosts(res.data);
-            setDiff(res.data?.length - totalPosts.length);
-        });
-        promise.catch(() => setDiff(0));
-    }, 15000);
+  function handleClickNotification() {
+    const totalPostsUpdated = [...newPosts];
+    const itemsToUpdate = newPosts?.splice(0, diff);
+    setTotalPosts(totalPostsUpdated);
+    setPostsArray([...itemsToUpdate, ...postsArray]);
+    setDiff(0);
+  }
 
-    function handleClickNotification() {
-        const totalPostsUpdated = [...newPosts];
-        const itemsToUpdate = newPosts?.splice(0, diff);
-        console.log("itemsToUpdate", itemsToUpdate);
-        setTotalPosts(totalPostsUpdated);
-        setPostsArray([...itemsToUpdate, ...postsArray]);
-        setDiff(0);
-    }
-
-    return (
-        <NotificationBar onClick={handleClickNotification} show={diff > 0}>
-            <span>
-                {diff} new {diff === 1 ? "post" : "posts"}, load more!
-            </span>
-            <IoReloadCircleSharp className="reload-icon" />
-        </NotificationBar>
-    );
+  return (
+    <NotificationBar onClick={handleClickNotification} show={diff > 0}>
+      <span>
+        {diff} new {diff === 1 ? "post" : "posts"}, load more!
+      </span>
+      <IoReloadCircleSharp className="reload-icon" />
+    </NotificationBar>
+  );
 }
