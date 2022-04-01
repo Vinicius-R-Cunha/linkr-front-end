382e51cbf763f46fa9e17c0ba91b6b00aa338604
kethllen <kety_andrey@hotmail.com>
2022-04-01 10:16:46 -0300

    corrigindo conflitos

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -20,109 +20,69 @@ import ViewComments from "../ViewComments";
 import PostsContext from "../../contexts/PostsContext";
 import { TiArrowSync } from "react-icons/ti";
 export default function Timeline({
-    showPublish,
-    route,
-    mainTitle,
-    hashtags,
-    setHashtags,
-    setIsValidUser,
+  showPublish,
+  route,
+  mainTitle,
+  hashtags,
+  setHashtags,
+  setIsValidUser,
 }) {
-<<<<<<< HEAD
   const { token, setImage, setName, setId } = useContext(UserContext);
   const { comments } = useContext(CommentsContext);
   const { posts } = useContext(PostsContext);
   const navigate = useNavigate();
 
-  const [postsArray, setPostsArray] = useState();
+  const [postsArray, setPostsArray] = useState([]);
   const [postsState, setPostsState] = useState("loading");
   const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
   const [repostModalIsOpen, setRepostModalIsOpen] = useState(false);
   const [postId, setPostId] = useState();
-  const route2 = route.split("/", 1).join();
-  console.log(route2);
+
   const renderPage = useCallback(
     async (route) => {
       try {
+        const followers = await api.checkFollowers(token);
         const posts = await api.getPosts(route, token);
         const hashtagsApi = await api.getHashtags(token);
 
         setPostsArray(posts.data);
 
-        if (posts?.data.length === 0) {
-          setPostsState("empty");
-=======
-    const { token, setImage, setName, setId } = useContext(UserContext);
-    const { comments } = useContext(CommentsContext);
-    const { posts } = useContext(PostsContext);
-    const navigate = useNavigate();
-
-    const [postsArray, setPostsArray] = useState([]);
-    const [postsState, setPostsState] = useState("loading");
-    const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
-    const [repostModalIsOpen, setRepostModalIsOpen] = useState(false);
-    const [postId, setPostId] = useState();
-
-    const renderPage = useCallback(
-        async (route) => {
-            try {
-                const followers = await api.checkFollowers(token);
-                const posts = await api.getPosts(route, token);
-                const hashtagsApi = await api.getHashtags(token);
-
-                setPostsArray(posts.data);
-
-                if (posts?.data.length !== 0) {
-                    setPostsState("full");
-                } else if (followers?.data.length === 0) {
-                    setPostsState("no-followers");
-                } else {
-                    setPostsState("empty");
-                }
-
-                setHashtags([...hashtagsApi.data]);
-                setIsValidUser(true);
-            } catch (error) {
-                setPostsState("error");
-            }
-        },
-        // eslint-disable-next-line
-        [token, setHashtags, setIsValidUser, hashtags]
-    );
-
-    const getUser = useCallback(async () => {
-        try {
-            const response = await api.getUserInfos(token);
-            setImage(response?.data.image);
-            setName(response?.data.name);
-            setId(response?.data.id);
-        } catch (error) {
-            console.log(error);
-        }
-    }, [setId, setImage, setName, token]);
-
-    useEffect(() => {
-        if (token) {
-            getUser();
-            renderPage(route);
->>>>>>> 927363dbf696a6053e66ac44f0a4380a4fea799c
+        if (posts?.data.length !== 0) {
+          setPostsState("full");
+        } else if (followers?.data.length === 0) {
+          setPostsState("no-followers");
         } else {
-            navigate("/");
+          setPostsState("empty");
         }
-        // eslint-disable-next-line
-    }, [token, route, getUser]);
 
-    function openDeleteModal() {
-        setDeleteModalIsOpen(true);
-    }
+        setHashtags([...hashtagsApi.data]);
+        setIsValidUser(true);
+      } catch (error) {
+        setPostsState("error");
+      }
+    },
+    // eslint-disable-next-line
+    [token, setHashtags, setIsValidUser, hashtags]
+  );
 
-    function closeDeleteModal() {
-        setDeleteModalIsOpen(false);
+  const getUser = useCallback(async () => {
+    try {
+      const response = await api.getUserInfos(token);
+      setImage(response?.data.image);
+      setName(response?.data.name);
+      setId(response?.data.id);
+    } catch (error) {
+      console.log(error);
     }
+  }, [setId, setImage, setName, token]);
 
-    function openRepostModal() {
-        setRepostModalIsOpen(true);
+  useEffect(() => {
+    if (token) {
+      getUser();
+      renderPage(route);
+    } else {
+      navigate("/");
     }
-<<<<<<< HEAD
     // eslint-disable-next-line
   }, [token, route, getUser]);
 
@@ -141,7 +101,7 @@ export default function Timeline({
   function closeRepostModal() {
     setRepostModalIsOpen(false);
   }
-  console.log(comments);
+
   return (
     <>
       <PostsContainer>
@@ -162,129 +122,82 @@ export default function Timeline({
           renderPage={renderPage}
           route={route}
         />
-        {route2 == "users"}
+
         <h1 className="timeline-title">{mainTitle}</h1>
 
         {showPublish && <Publish renderPage={renderPage} route={route} />}
 
-        {postsState === "full" &&
-          postsArray.map((post) => {
-            return (
-              <ContainerComments>
-                <Post key={post?.id}>
-                  <PostLeftContent
-                    post={post}
-=======
-
-    function closeRepostModal() {
-        setRepostModalIsOpen(false);
-    }
-
-    return (
-        <>
-            <PostsContainer>
-                <ToastContainer />
-                <SearchBarMobile />
-                <DeleteConfirmationModal
-                    postId={postId}
-                    closeModal={closeDeleteModal}
-                    modalIsOpen={deleteModalIsOpen}
->>>>>>> 927363dbf696a6053e66ac44f0a4380a4fea799c
-                    renderPage={renderPage}
-                    route={route}
-                />
-
-                <RepostConfirmationModal
-                    postId={postId}
-                    closeModal={closeRepostModal}
-                    modalIsOpen={repostModalIsOpen}
-                    renderPage={renderPage}
-                    route={route}
-                />
-
-                <h1 className="timeline-title">{mainTitle}</h1>
-
-                {showPublish && (
-                    <Publish renderPage={renderPage} route={route} />
-                )}
-
-                {/* <NewPostsNotificationBar
+        {/* <NewPostsNotificationBar
                     route={route}
                     token={token}
                     postsArray={postsArray}
                     setPostsArray={setPostsArray}
                 /> */}
-                {postsArray?.length === 0 ? (
-                    <></>
-                ) : (
-                    <ScrollContainer
+        {postsArray?.length === 0 ? (
+          <></>
+        ) : (
+          <ScrollContainer
+            route={route}
+            token={token}
+            postsArray={postsArray}
+            setPostsArray={setPostsArray}
+          >
+            {postsState === "full" &&
+              postsArray.map((post) => {
+                return (
+                  <ContainerComments key={post?.id}>
+                    {post?.repostId && (
+                      <Repost>
+                        <TiArrowSync className="reposted-icon" />
+                        <p>
+                          Re-posted by <span>{post?.name}</span>
+                        </p>
+                      </Repost>
+                    )}
+
+                    <Post>
+                      <PostLeftContent
+                        post={post}
+                        renderPage={renderPage}
                         route={route}
-                        token={token}
-                        postsArray={postsArray}
-                        setPostsArray={setPostsArray}
-                    >
-                        {postsState === "full" &&
-                            postsArray.map((post) => {
-                                return (
-                                    <ContainerComments key={post?.id}>
-                                        {post?.repostId && (
-                                            <Repost>
-                                                <TiArrowSync className="reposted-icon" />
-                                                <p>
-                                                    Re-posted by{" "}
-                                                    <span>{post?.name}</span>
-                                                </p>
-                                            </Repost>
-                                        )}
+                        openModal={openRepostModal}
+                        setPostId={setPostId}
+                      />
 
-                                        <Post>
-                                            <PostLeftContent
-                                                post={post}
-                                                renderPage={renderPage}
-                                                route={route}
-                                                openModal={openRepostModal}
-                                                setPostId={setPostId}
-                                            />
-
-                                            <PostContent
-                                                post={post}
-                                                renderPage={renderPage}
-                                                route={route}
-                                                openModal={openDeleteModal}
-                                                setPostId={setPostId}
-                                            />
-                                        </Post>
-                                        {comments !== null &&
-                                            posts === post.id && (
-                                                <ViewComments
-                                                    postId={post.id}
-                                                />
-                                            )}
-                                    </ContainerComments>
-                                );
-                            })}
-                    </ScrollContainer>
-                )}
-                {postsState === "loading" && (
-                    <p className="loading-message">Loading...</p>
-                )}
-                {postsState === "no-followers" && (
-                    <p className="get-error-message">
-                        You don't follow anyone yet. Search for new friends!
-                    </p>
-                )}
-                {postsState === "empty" && (
-                    <p className="get-error-message">
-                        No posts found from your friends
-                    </p>
-                )}
-                {postsState === "error" && (
-                    <p className="get-error-message">
-                        An error occured while trying to fetch the posts, please
-                        refresh the page
-                    </p>
-                )}
-            </PostsContainer>
-        </>
-    );
+                      <PostContent
+                        post={post}
+                        renderPage={renderPage}
+                        route={route}
+                        openModal={openDeleteModal}
+                        setPostId={setPostId}
+                      />
+                    </Post>
+                    {comments !== null && posts === post.id && (
+                      <ViewComments postId={post.id} />
+                    )}
+                  </ContainerComments>
+                );
+              })}
+          </ScrollContainer>
+        )}
+        {postsState === "loading" && (
+          <p className="loading-message">Loading...</p>
+        )}
+        {postsState === "no-followers" && (
+          <p className="get-error-message">
+            You don't follow anyone yet. Search for new friends!
+          </p>
+        )}
+        {postsState === "empty" && (
+          <p className="get-error-message">No posts found from your friends</p>
+        )}
+        {postsState === "error" && (
+          <p className="get-error-message">
+            An error occured while trying to fetch the posts, please refresh the
+            page
+          </p>
+        )}
+      </PostsContainer>
+    </>
+  );
 }
