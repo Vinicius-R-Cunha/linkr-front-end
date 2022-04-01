eb42610d8b03ece59b58220e332c7c9f5b7bd321
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-04-01 15:51:30 -0300

    fix: notification bar just appear on /timeline

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -23,235 +23,260 @@ import { TiArrowSync } from "react-icons/ti";
 import reactDom from "react-dom";
 import { FaGratipay } from "react-icons/fa";
 export default function Timeline({
-  showPublish,
-  route,
-  mainTitle,
-  hashtags,
-  setHashtags,
-  setIsValidUser,
-  visitedUserId
+    showPublish,
+    route,
+    mainTitle,
+    hashtags,
+    setHashtags,
+    setIsValidUser,
+    visitedUserId,
 }) {
+    const { token, setImage, setName, setId } = useContext(UserContext);
+    const { comments } = useContext(CommentsContext);
+    const { posts } = useContext(PostsContext);
+    const navigate = useNavigate();
 
-  const { token, setImage, setName, setId } = useContext(UserContext);
-  const { comments } = useContext(CommentsContext);
-  const { posts } = useContext(PostsContext);
-  const navigate = useNavigate();
+    const [postsArray, setPostsArray] = useState([]);
+    const [postsState, setPostsState] = useState("loading");
+    const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+    const [repostModalIsOpen, setRepostModalIsOpen] = useState(false);
+    const [postId, setPostId] = useState();
+    const [followedUser, setFollowedUser] = useState();
+    const [disabledButton, setDisabledButton] = useState(false);
+    const [currentPost, setCurrentPost] = useState();
+    const [totalPosts, setTotalPosts] = useState([]);
 
-  const [postsArray, setPostsArray] = useState([]);
-  const [postsState, setPostsState] = useState("loading");
-  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
-  const [repostModalIsOpen, setRepostModalIsOpen] = useState(false);
-  const [postId, setPostId] = useState();
-  const [followedUser, setFollowedUser] = useState();
-  const [disabledButton, setDisabledButton] = useState(false);
-  const [currentPost, setCurrentPost] = useState();
-  const [totalPosts, setTotalPosts] = useState([]);
-
-  const renderPage = useCallback(
-    async (route) => {
-      try {
-        const followers = await api.checkFollowers(token);
-        const posts = await api.getPosts(route, token);
-        const hashtagsApi = await api.getHashtags(token);
-        const allPosts = await api.getPosts(
+    const renderPage = useCallback(
+        async (route) => {
+            try {
+                const followers = await api.checkFollowers(token);
+                const posts = await api.getPosts(route, token);
+                const hashtagsApi = await api.getHashtags(token);
+                const allPosts = await api.getPosts(
                     route + "?noLimit=true",
                     token
                 );
-        setTotalPosts(allPosts.data);
-        setPostsArray(posts.data);
+                setTotalPosts(allPosts.data);
+                setPostsArray(posts.data);
 
-        if (posts?.data.length !== 0) {
-          setPostsState("full");
-        } else if (followers?.data.length === 0) {
-          setPostsState("no-followers");
-        } else {
-          setPostsState("empty");
+                if (posts?.data.length !== 0) {
+                    setPostsState("full");
+                } else if (followers?.data.length === 0) {
+                    setPostsState("no-followers");
+                } else {
+                    setPostsState("empty");
+                }
+
+                setHashtags([...hashtagsApi.data]);
+                setIsValidUser(true);
+            } catch (error) {
+                setPostsState("error");
+            }
+        },
+        // eslint-disable-next-line
+        [token, setHashtags, setIsValidUser, hashtags]
+    );
+
+    const getUser = useCallback(async () => {
+        try {
+            const response = await api.getUserInfos(token);
+            setImage(response?.data.image);
+            setName(response?.data.name);
+            setId(response?.data.id);
+        } catch (error) {
+            console.log(error);
         }
+    }, [setId, setImage, setName, token]);
 
-        setHashtags([...hashtagsApi.data]);
-        setIsValidUser(true);
-      } catch (error) {
-        setPostsState("error");
-      }
-    },
-    // eslint-disable-next-line
-    [token, setHashtags, setIsValidUser, hashtags]
-  );
+    useEffect(() => {
+        if (token) {
+            getUser();
+            verifyIfFollow();
+            renderPage(route);
+        } else {
+            navigate("/");
+        }
+        // eslint-disable-next-line
+    }, [token, route, getUser]);
 
-  const getUser = useCallback(async () => {
-    try {
-      const response = await api.getUserInfos(token);
-      setImage(response?.data.image);
-      setName(response?.data.name);
-      setId(response?.data.id);
-    } catch (error) {
-      console.log(error);
+    function openDeleteModal() {
+        setDeleteModalIsOpen(true);
     }
-  }, [setId, setImage, setName, token]);
 
-  useEffect(() => {
-    if (token) {
-      getUser();
-      verifyIfFollow()
-      renderPage(route);
-    } else {
-      navigate("/");
+    function closeDeleteModal() {
+        setDeleteModalIsOpen(false);
     }
-    // eslint-disable-next-line
-  }, [token, route, getUser]);
 
-  function openDeleteModal() {
-    setDeleteModalIsOpen(true);
-  }
+    function openRepostModal() {
+        setRepostModalIsOpen(true);
+    }
 
-  function closeDeleteModal() {
-    setDeleteModalIsOpen(false);
-  }
+    function closeRepostModal() {
+        setRepostModalIsOpen(false);
+    }
 
-  function openRepostModal() {
-    setRepostModalIsOpen(true);
-  }
+    async function toggleFollow() {
+        try {
+            setDisabledButton(true);
+            await api.toggleFollow(visitedUserId, token);
+            const response = await api.verifyIfFollow(visitedUserId, token);
+            setFollowedUser(response?.data);
+            setDisabledButton(false);
+        } catch (error) {
+            return Swal.fire({
+                icon: "error",
+                title: "Ops...",
+                text: "Não foi possível realizar esta ação! Tente mais tarde!",
+            });
+        }
+    }
 
-  function closeRepostModal() {
-    setRepostModalIsOpen(false);
-  }
-
-  async function toggleFollow() {
-      try {
-        setDisabledButton(true)
-        await api.toggleFollow(visitedUserId, token);
-        const response = await api.verifyIfFollow(visitedUserId, token);
-        setFollowedUser(response?.data)
-        setDisabledButton(false)            
-      } catch (error) {
-          return Swal.fire({
-              icon: "error",
-              title: "Ops...",
-              text: "Não foi possível realizar esta ação! Tente mais tarde!"
-          });
-      };
-  };
-
-  async function verifyIfFollow() {
+    async function verifyIfFollow() {
         try {
             const response = await api.verifyIfFollow(visitedUserId, token);
-            setFollowedUser(response?.data)
-            
+            setFollowedUser(response?.data);
         } catch (error) {
-          console.log(error)
-        };
-  };  
-
-  return (
-    <>
-      <PostsContainer>
-        <ToastContainer />
-        <SearchBarMobile />
-        <DeleteConfirmationModal
-          postId={postId}
-          closeModal={closeDeleteModal}
-          modalIsOpen={deleteModalIsOpen}
-          renderPage={renderPage}
-          route={route}
-        />
-
-        <RepostConfirmationModal
-          postId={postId}
-          closeModal={closeRepostModal}
-          modalIsOpen={repostModalIsOpen}
-          renderPage={renderPage}
-          route={route}
-        />
-
-        {mainTitle.includes("'s posts") ?
-          <div className="usernameAndFollowButton">
-            <h1 className="timeline-title">{mainTitle}</h1>
-
-            {disabledButton ? <button className="followButton" disabled={disabledButton} onClick={toggleFollow}> loading... </button> : <button className="followButton" disabled={disabledButton} onClick={toggleFollow}> {followedUser} </button> }
-            
-          </div>  
-        :
-        <h1 className="timeline-title">{mainTitle}</h1>
+            console.log(error);
         }
+    }
 
-        {showPublish && <Publish renderPage={renderPage} route={route} />}
-
-
-                <NewPostsNotificationBar
+    return (
+        <>
+            <PostsContainer>
+                <ToastContainer />
+                <SearchBarMobile />
+                <DeleteConfirmationModal
+                    postId={postId}
+                    closeModal={closeDeleteModal}
+                    modalIsOpen={deleteModalIsOpen}
+                    renderPage={renderPage}
                     route={route}
-                    token={token}
-                    postsArray={postsArray}
-                    setPostsArray={setPostsArray}
-
-                    totalPosts={totalPosts}
-                    setTotalPosts={setTotalPosts}
                 />
+
+                <RepostConfirmationModal
+                    postId={postId}
+                    closeModal={closeRepostModal}
+                    modalIsOpen={repostModalIsOpen}
+                    renderPage={renderPage}
+                    route={route}
+                />
+
+                {mainTitle.includes("'s posts") ? (
+                    <div className="usernameAndFollowButton">
+                        <h1 className="timeline-title">{mainTitle}</h1>
+
+                        {disabledButton ? (
+                            <button
+                                className="followButton"
+                                disabled={disabledButton}
+                                onClick={toggleFollow}
+                            >
+                                {" "}
+                                loading...{" "}
+                            </button>
+                        ) : (
+                            <button
+                                className="followButton"
+                                disabled={disabledButton}
+                                onClick={toggleFollow}
+                            >
+                                {" "}
+                                {followedUser}{" "}
+                            </button>
+                        )}
+                    </div>
+                ) : (
+                    <h1 className="timeline-title">{mainTitle}</h1>
+                )}
+
+                {showPublish && (
+                    <Publish renderPage={renderPage} route={route} />
+                )}
+
+                {route === "timeline" ? (
+                    <NewPostsNotificationBar
+                        route={route}
+                        token={token}
+                        postsArray={postsArray}
+                        setPostsArray={setPostsArray}
+                        totalPosts={totalPosts}
+                        setTotalPosts={setTotalPosts}
+                    />
+                ) : (
+                    <></>
+                )}
                 {postsArray?.length === 0 ? (
                     <></>
                 ) : (
                     <ScrollContainer
-            route={route}
-            token={token}
-            postsArray={postsArray}
-            setPostsArray={setPostsArray}
-          >
-            {postsState === "full" &&
-              postsArray.map((post) => {
-                return (
-                  <ContainerComments key={post?.id}>
-                    {post?.repostId && (
-                      <Repost>
-                        <TiArrowSync className="reposted-icon" />
-                        <p>
-                          Re-posted by <span>{post?.name}</span>
-                        </p>
-                      </Repost>
-                    )}
-
-                    <Post>
-                      <PostLeftContent
-                        post={post}
-                        renderPage={renderPage}
                         route={route}
-                        openModal={openRepostModal}
-                        setPostId={setPostId}
-                        setCurrentPost={setCurrentPost}
-                      />
+                        token={token}
+                        postsArray={postsArray}
+                        setPostsArray={setPostsArray}
+                    >
+                        {postsState === "full" &&
+                            postsArray.map((post) => {
+                                return (
+                                    <ContainerComments key={post?.id}>
+                                        {post?.repostId && (
+                                            <Repost>
+                                                <TiArrowSync className="reposted-icon" />
+                                                <p>
+                                                    Re-posted by{" "}
+                                                    <span>{post?.name}</span>
+                                                </p>
+                                            </Repost>
+                                        )}
 
-                      <PostContent
-                        post={post}
-                        renderPage={renderPage}
-                        route={route}
-                        openModal={openDeleteModal}
-                        setPostId={setPostId}
-                      />
-                    </Post>
-                    {comments !== null && posts === post.id && (
-                      <ViewComments postId={post.id} />
-                    )}
-                  </ContainerComments>
-                );
-              })}
-          </ScrollContainer>
-        )}
-        {postsState === "loading" && (
-          <p className="loading-message">Loading...</p>
-        )}
-        {postsState === "no-followers" && (
-          <p className="get-error-message">
-            You don't follow anyone yet. Search for new friends!
-          </p>
-        )}
-        {postsState === "empty" && (
-          <p className="get-error-message">No posts found from your friends</p>
-        )}
-        {postsState === "error" && (
-          <p className="get-error-message">
-            An error occured while trying to fetch the posts, please refresh the
-            page
-          </p>
-        )}
-      </PostsContainer>
-    </>
-  );
+                                        <Post>
+                                            <PostLeftContent
+                                                post={post}
+                                                renderPage={renderPage}
+                                                route={route}
+                                                openModal={openRepostModal}
+                                                setPostId={setPostId}
+                                                setCurrentPost={setCurrentPost}
+                                            />
+
+                                            <PostContent
+                                                post={post}
+                                                renderPage={renderPage}
+                                                route={route}
+                                                openModal={openDeleteModal}
+                                                setPostId={setPostId}
+                                            />
+                                        </Post>
+                                        {comments !== null &&
+                                            posts === post.id && (
+                                                <ViewComments
+                                                    postId={post.id}
+                                                />
+                                            )}
+                                    </ContainerComments>
+                                );
+                            })}
+                    </ScrollContainer>
+                )}
+                {postsState === "loading" && (
+                    <p className="loading-message">Loading...</p>
+                )}
+                {postsState === "no-followers" && (
+                    <p className="get-error-message">
+                        You don't follow anyone yet. Search for new friends!
+                    </p>
+                )}
+                {postsState === "empty" && (
+                    <p className="get-error-message">
+                        No posts found from your friends
+                    </p>
+                )}
+                {postsState === "error" && (
+                    <p className="get-error-message">
+                        An error occured while trying to fetch the posts, please
+                        refresh the page
+                    </p>
+                )}
+            </PostsContainer>
+        </>
+    );
 }
